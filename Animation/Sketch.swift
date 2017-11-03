6a13a15023971b9e017e9c32998573f517b1e8cb
Da, Annie <ada@lcs-mba3107.lcs.on.ca>
2017-11-03 11:15:30 -0400

    half

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -1,34 +1,48 @@
+
 import Foundation
 
 class Sketch : NSObject {
     
-    // NOTE: Every sketch must contain an object of type Canvas named 'canvas'
-    //       Therefore, the line immediately below must always be present.
     let canvas : Canvas
     
-    // Position of circle
+    // Position of candy
     var x : Int
+    
+    // Position of candy
+    var y : Int
     
     // This function runs once
     override init() {
         
         // Create canvas object – specify size
-        canvas = Canvas(width: 500, height: 500)
+        canvas = Canvas(width: 800, height: 600)
         
-        // Set starting position
-        x = 250
+        // Set starting horizontal position
+        x = 0
+        
+        // Set starting vertical position
+        y = 0
         
     }
     
     // Runs in a loop, forever, to create the animated effect
     func draw() {
         
-        // Change position
-        x += 1
+        // Clear the canvas
+        canvas.fillColor = Color.white
+        canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 800, height: 600)
         
-        // Draw an ellipse in the middle of the canvas
-        canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50)
+        // Move from left to right
+        x += 1
+
+    //sine function
+    let xInRadians = Double(x)*Double(Double.pi)/180.0
+    y = Int(100*sin(xInRadians)+300)
+        
+     // move the axes
+        canvas.translate(byX: x, byY: y)
         
     }
     
 }
+    
--- CanvasTestArea.playground/Contents.swift
+++ CanvasTestArea.playground/Contents.swift
@@ -15,7 +15,68 @@ import Cocoa
 import PlaygroundSupport
 
 // Create canvas
-let canvas = Canvas(width: 400, height: 300)
+let canvas = Canvas(width: 800, height: 600)
+
+/*:
+ ## Add your code below
+ 
+ Be sure to write human-readable code.
+ 
+ Use whitespace and comments as appropriate.
+ */
+// Replace this comment with your first comment – what is the goal of the code you're about to write?
+
+//no borders
+canvas.drawShapesWithBorders = false
+
+//move axis
+canvas.translate(byX: 400, byY: 300)
+
+//colour
+canvas.fillColor = Color.orange
+//draw two sides of the candy
+canvas.drawEllipse(centreX: 45, centreY: 20, width: 60, height: 60)
+canvas.drawEllipse(centreX: -45, centreY: -20, width: 60, height: 60)
+//color
+canvas.fillColor = Color.white
+//rectangles
+canvas.drawRectangle(centreX: 60, centreY: -18, width: 65, height: 40)
+canvas.drawRectangle(centreX: -60, centreY: 18, width: 65, height: 40)
+
+//tilted white rectangles
+canvas.rotate(by: 50)
+//color
+canvas.fillColor = Color.white
+//rectangles
+canvas.drawRectangle(centreX: -35, centreY: -25, width: 60, height: 50)
+canvas.drawRectangle(centreX: 35, centreY: 25, width: 60, height: 50)
+
+//color
+canvas.fillColor = Color.blue
+//body of the candy
+canvas.drawEllipse(centreX: 0, centreY: 0, width: 60, height: 60)
+
+
+//blue body
+canvas.rotate(by: 0)
+//color
+canvas.fillColor = Color.purple
+//ellipse
+canvas.drawEllipse(centreX: 0, centreY: 20, width: 8, height: 8)
+canvas.drawEllipse(centreX: -10, centreY: -10, width: 8, height: 8)
+canvas.drawEllipse(centreX: 0, centreY: -5, width: 8, height: 8)
+canvas.drawEllipse(centreX: 10, centreY: 0, width: 8, height: 8)
+
+
+//tilted white rectangles
+canvas.rotate(by: 70)
+//color
+canvas.fillColor = Color.white
+//rectangles
+canvas.drawRectangle(centreX: 3, centreY: 85, width: 60, height: 50)
+canvas.drawRectangle(centreX: -3, centreY: -85, width: 60, height: 50)
+
+
 
 /*:
  ## Add your code below
