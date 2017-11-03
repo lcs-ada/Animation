b72b96ff6267fd62472a91ffe178d23007dc0e6e
Da, Annie <ada@lcs-mba3107.lcs.on.ca>
2017-11-03 11:27:22 -0400

    FIVE CIRCLE CHALLENGE- COMPLETE:-)

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -20,26 +20,35 @@ class Sketch : NSObject {
         
     }
     
+
     // Runs in a loop, forever, to create the animated effect
     func draw() {
         
         // Change position
         x += 1
+        print(500 - x)
+        
+        canvas.drawShapesWithBorders = false
         
         // Draw an ellipse in the middle of the canvas
+        canvas.fillColor = Color.purple
         canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50)
+        
         //draw an ellipse in on the first row
-        canvas.drawEllipse(centreX: x, centreY: 350, width: 50, height: 50)
+         canvas.fillColor = Color.orange
+        canvas.drawEllipse(centreX: 500 - x, centreY: 350, width: 50, height: 50)
         
         //draw an ellipse in on the second row
+         canvas.fillColor = Color.green
         canvas.drawEllipse(centreX: x, centreY: 450, width: 50, height: 50)
         
         //draw an ellipse in on the fourth row
-        canvas.drawEllipse(centreX: x, centreY: 150, width: 50, height: 50)
+         canvas.fillColor = Color.blue
+        canvas.drawEllipse(centreX: 500 - x, centreY: 150, width: 50, height: 50)
         
         //draw an ellipse in on the fifth row
+         canvas.fillColor = Color.red
         canvas.drawEllipse(centreX: x, centreY: 50, width: 50, height: 50)
-        
     }
     
 }
