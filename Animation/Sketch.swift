15562d96e2d0d421790767216922483c1f047d74
Da, Annie <ada@lcs-mba3107.local>
2017-11-14 11:24:18 -0500

    four corners = complete:)

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -2,33 +2,39 @@ import Foundation
 
 class Sketch : NSObject {
     
-    // NOTE: Every sketch must contain an object of type Canvas named 'canvas'
-    //       Therefore, the line immediately below must always be present.
     let canvas : Canvas
     
-    // Position of circle
     var x : Int
+    var y : Int
+    //this function runs once
+    override init(){
+        
+    //create canvas object - specify size
+        canvas = Canvas(width:500, height: 500)
     
-    // This function runs once
-    override init() {
-        
-        // Create canvas object – specify size
-        canvas = Canvas(width: 500, height: 500)
-        
-        // Set starting position
+        //set starting point
         x = 250
-        
+        y = 250
+          canvas.drawShapesWithBorders = false
     }
-    
     // Runs in a loop, forever, to create the animated effect
     func draw() {
         
         // Change position
         x += 1
+        y = 1
         
-        // Draw an ellipse in the middle of the canvas
-        canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50)
-        
-    }
     
+    canvas.fillColor = Color.red
+canvas.drawEllipse(centreX: x, centreY: x + y , width: 50, height: 50)
+    
+    canvas.fillColor = Color.blue
+canvas.drawEllipse(centreX: x, centreY: 500 - x , width: 50, height: 50)
+
+    canvas.fillColor = Color.green
+canvas.drawEllipse(centreX: -x + 500, centreY: x , width: 50, height: 50)
+    
+    canvas.fillColor = Color.yellow
+canvas.drawEllipse(centreX: -x + 500, centreY: -x + 500 , width: 50, height: 50)
+}
 }
